c1e9fb9a56e27e5d70ce078e4c351393bf50afb3
Tribunskiy <nekit2326@gmail.com>
2017-06-29 08:03:04 -0700

    on Text handler (add)

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const TelegramBot = require('node-telegram-bot-api');
 const config = require('config');
+const low = require('lowdb');
+
+const words = low('words.json');
 
 const bot = new TelegramBot(config.get('token'), {polling: true});
 
-bot.on('message', (msg) => {
-    const chatId = msg.chat.id;
-
-    bot.sendMessage(chatId, `Hello ${msg.from.first_name}!`);
+bot.onText(/\/add ([A-z]+) ([А-я]+)/, (msg, match) => {
+    words.get('learn')
 });
